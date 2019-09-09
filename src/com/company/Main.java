dada47de4b66f4129b39c6ae0e0d56869fef9b9b
xinzi <ziheng.xin@ssfs.org>
2019-09-09 14:05:16 -0400

    added the third statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("hello, world!");
         System.out.println("This is my first CS course");
+        System.out.println("this is \"Charlie's\" computer");
+        System.out.println("I am splitting this\n over two lines");
     }
 }
