75f0233d8b54cad6814bde79f2732830fb81ae95
xinzi <ziheng.xin@ssfs.org>
2019-09-10 14:45:12 -0400

    9/10 classwork

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,9 @@
 package com.company;
 
-c
+public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
         System.out.println("hello, world!");
         System.out.println("This is my first CS course");
         System.out.println("this is \"Charlie's\" computer");
---
+++ src/com/company/sep10.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class sep10 {
+
+    public static void main(String[] args) {
+
+        System.out.print("14:43");
+        System.out.println("09/10/2019");
+    }
+}
