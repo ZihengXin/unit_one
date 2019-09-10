81545f51a0c1d160b6cbbf393b98976bd8824789
xinzi <ziheng.xin@ssfs.org>
2019-09-10 14:57:26 -0400

    9/10 classwork part 1

--- src/com/company/sep10.java
+++ src/com/company/sep10.java
@@ -4,7 +4,11 @@ public class sep10 {
 
     public static void main(String[] args) {
 
-        System.out.print("14:43");
+        System.out.println("14:43");
         System.out.println("09/10/2019");
+        System.out.println("It is a great\ntime to be a\ncomputer programmer!");
+        System.out.print("It is a great " ) ;
+        System.out.print("time to be a");
+        System.out.print("computer programmer!");
     }
 }
