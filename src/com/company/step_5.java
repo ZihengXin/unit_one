4c9d9f2682e9c2fa1d4ee01815a137a79a567dfd
xinzi <ziheng.xin@ssfs.org>
2019-09-12 08:32:39 -0400

    9/12 classwork part5

---
+++ src/com/company/step_5.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class step_5 {
+
+    public static void main(String[] args){
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+
+    }
+
+}
