7ff229b1e61212fa6c8f2379102324901793dd9c
xinzi <ziheng.xin@ssfs.org>
2019-09-13 10:20:47 -0400

    9/13 painting a room (not done yet)

--- src/com/company/Painting_a_room.java
+++ src/com/company/Painting_a_room.java
@@ -11,8 +11,12 @@ public class Painting_a_room {
         int area_of_window=15;
         int number_of_windows=4;
         int total_area= length*width+2*length*height+2*width*height;
-        int area_need_to_be_paint= total_area-number_of_door*area_of_door-number_of_windows*area_of_window;
-        int gallons_needed= area_need_to_be_paint/area_per_gallon;
-        System.out.print("You will need " + gallons_needed + " gallons of paint");
+        double area_need_to_be_paint= total_area-(number_of_door*area_of_door+number_of_windows*area_of_window);
+        int gallons_needed;
+        double remainder= area_need_to_be_paint%area_per_gallon;
+        if(remainder==0){
+            int result=area_need_to_be_paint/area_per_gallon}else{int result=area_need_to_be_paint%area_per_gallon+1}
+        }
+        System.out.print("You will need " + int result + " gallons of paint");
     }
 }
