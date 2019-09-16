082b5b04e5ae475b4dc88c417742c154e370178f
xinzi <ziheng.xin@ssfs.org>
2019-09-16 10:38:49 -0400

    9/16 painting a room (Finally done)

--- src/com/company/Painting_a_room.java
+++ src/com/company/Painting_a_room.java
@@ -1,22 +1,25 @@
 package com.company;
 
 public class Painting_a_room {
-    public static void main(String[] args){
-        final int area_per_gallon=350;
-        int length=32;
-        int width=40;
-        int height=16;
-        int area_of_door=20;
-        int number_of_door=2;
-        int area_of_window=15;
-        int number_of_windows=4;
-        int total_area= length*width+2*length*height+2*width*height;
-        double area_need_to_be_paint= total_area-(number_of_door*area_of_door+number_of_windows*area_of_window);
+    public static void main(String[] args) {
+        final int area_per_gallon = 350;
+        double length = 32;
+        double width = 40;
+        double height = 16;
+        int AREA_OF_DOOR = 20;
+        int NUMBER_OF_DOOR = 2;
+        int AREA_OF_WINDOW = 15;
+        int NUMBER_OF_WINDOWS = 4;
+        double total_area = length * width + 2 * length * height + 2 * width * height;
+        double area_need_to_be_paint = total_area - (NUMBER_OF_DOOR * AREA_OF_DOOR + NUMBER_OF_WINDOWS * AREA_OF_WINDOW);
         int gallons_needed;
-        double remainder= area_need_to_be_paint%area_per_gallon;
-        if(remainder==0){
-            int result=area_need_to_be_paint/area_per_gallon}else{int result=area_need_to_be_paint%area_per_gallon+1}
+        double remainder = area_need_to_be_paint % area_per_gallon;
+        double result=area_need_to_be_paint / area_per_gallon;
+        if (remainder == 0) {
+             result = (int)result;
+        } else {
+             result = (int)result + 1;
         }
-        System.out.print("You will need " + int result + " gallons of paint");
+        System.out.println("You will need " +  result + " gallons of paint");
     }
 }
