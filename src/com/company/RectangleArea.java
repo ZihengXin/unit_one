b2d4ef15b1021462cba127f1898d1e224dc2f6e8
xinzi <ziheng.xin@ssfs.org>
2019-09-17 12:47:36 -0400

    Rectangle Area with new input

--- src/com/company/RectangleArea.java
+++ src/com/company/RectangleArea.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RectangleArea {
     public static void main(String[] args) {
-        int length = 14;
-        int width = 6;
-        int area = length * width;
+        Scanner Ahri= new Scanner (System.in);
+        Scanner Annie= new Scanner (System.in);
+        System.out.println("Type in the length in numbers: ");
+        Double length = Ahri.nextDouble();
+        System.out.println("Type in the width: ");
+        Double width = Annie.nextDouble();
+        Double area = length * width;
         System.out.print("Area = ");
         System.out.print(area);
     }
