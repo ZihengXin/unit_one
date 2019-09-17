c5a109810111c752c9c3da4e34526de98b190cfe
xinzi <ziheng.xin@ssfs.org>
2019-09-17 12:32:40 -0400

    tempConversion with new input

--- src/com/company/TempConversion.java
+++ src/com/company/TempConversion.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TempConversion {
     public static void main(String[] args){
+        System.out.println("Type in the Temp in Fahrenheit");
+        Scanner Neeko = new Scanner(System.in);
         double Fahrenheit;
         double Celsius;
-        Fahrenheit=79;
+        Fahrenheit=Neeko.nextDouble();
         Celsius=(Fahrenheit-32)/(1.8);
         System.out.println("Celsius = "+Celsius);
 
