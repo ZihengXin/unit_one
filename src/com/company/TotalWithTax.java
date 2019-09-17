6dda507387d373f637f6a00ac57207b9b5b2f40b
xinzi <ziheng.xin@ssfs.org>
2019-09-17 12:38:13 -0400

    total with tax with new input

--- src/com/company/TotalWithTax.java
+++ src/com/company/TotalWithTax.java
@@ -1,12 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TotalWithTax {
     public static void main(String[] args){
+        System.out.println("Type in the price of the goods");
+        Scanner Yasuo=new Scanner(System.in);
         final double TAXRARE= 0.0825;
         double price;
         double tax;
         double total;
-        price=52.75;
+        price=Yasuo.nextDouble();
         tax=price*TAXRARE;
         total= price+tax;
         System.out.println("Price: $"+price);
