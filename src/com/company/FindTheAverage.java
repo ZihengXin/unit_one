9c2f776ef3b8d04ea135436dc1559972ef2121a2
xinzi <ziheng.xin@ssfs.org>
2019-09-18 13:41:27 -0400

    average

--- src/com/company/FindTheAverage.java
+++ src/com/company/FindTheAverage.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class FindTheAverage {
     public static void main(String[] args){
         System.out.println("Enter four values. ");
-        System.out.println("Value 1: ");
+        System.out.print("Value 1: ");
         Scanner input= new Scanner(System.in);
         double value_one= input.nextDouble();
-        System.out.println("Value 2: ");
+        System.out.print("Value 2: ");
         double value_two= input.nextDouble();
-        System.out.println("Value 3: ");
+        System.out.print("Value 3: ");
         double value_three= input.nextDouble();
-        System.out.println("Value 4: ");
+        System.out.print("Value 4: ");
         double value_four= input.nextDouble();
         double result=0;
         result += value_one;
