be7808624de44ece51714d78c8ec2167e6532081
xinzi <ziheng.xin@ssfs.org>
2019-09-19 15:00:22 -0400

    round negative 9/19

---
+++ src/com/company/RoundNegatives.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class RoundNegatives {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a negative decimal number: ");
+        double input_number= input.nextDouble();
+        int result=(int)(input_number-0.5);
+        System.out.println(input_number+"rounded to a whole number is"+result);
+
+
+    }
+}
